2304a7bb9d9de7264d22cee81ab5402c79cb75f3
ali-jalaal <4487124+ali-jalaal@users.noreply.github.com>
2024-07-11 19:33:28 +0200

    Fix AuthorizationController package

--- client/src/main/kotlin/com/github/alijalaal/client/web/AuthorizationController.kt
+++ client/src/main/kotlin/com/github/alijalaal/client/web/AuthorizationController.kt
@@ -1,4 +1,4 @@
-package com.github.alijalaal.resourceserver.web
+package com.github.alijalaal.client.web
 
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Value
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
-
 
 @Controller
 class AuthorizationController(
