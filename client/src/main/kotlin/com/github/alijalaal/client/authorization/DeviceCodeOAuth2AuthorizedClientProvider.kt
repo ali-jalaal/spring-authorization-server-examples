2cef0afbd04ecaaaf482ad7ed450f8da0a94a986
ali-jalaal <4487124+ali-jalaal@users.noreply.github.com>
2024-07-12 23:23:09 +0200

    Add licence notice in client module's classes

--- client/src/main/kotlin/com/github/alijalaal/client/authorization/DeviceCodeOAuth2AuthorizedClientProvider.kt
+++ client/src/main/kotlin/com/github/alijalaal/client/authorization/DeviceCodeOAuth2AuthorizedClientProvider.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020-2023 the original author or authors.
+ * Modifications copyright (c) 2024 Ali Jalal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.alijalaal.client.authorization
 
 import jakarta.servlet.http.HttpServletRequest
@@ -15,7 +31,11 @@ import java.time.Duration
 import java.util.*
 import java.util.function.Function
 
-
+/**
+ * @author Steve Riesenberg
+ * @author Ali Jalal
+ * @since 1.1
+ */
 class DeviceCodeOAuth2AuthorizedClientProvider : OAuth2AuthorizedClientProvider {
   private var accessTokenResponseClient: OAuth2AccessTokenResponseClient<OAuth2DeviceGrantRequest> =
     OAuth2DeviceAccessTokenResponseClient()
--- client/src/test/kotlin/com/github/alijalaal/client/ClientApplicationTests.kt
+++ client/src/test/kotlin/com/github/alijalaal/client/ClientApplicationTests.kt
@@ -1,10 +1,10 @@
-package com.github.alijalaal.resourceserver
+package com.github.alijalaal.client
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class ResourceServerApplicationTests {
+class ClientApplicationTests {
 
   @Test
   fun contextLoads() {
